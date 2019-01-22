0406574b443e1aedcbfe164215b27674d360fde2
Dristi Marasini <s533985@NWMISSOURI.EDU>
2019-01-22 13:58:23 -0600

    Variables created

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GFeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************Graduate Semester Fee Calculator App******************");
+            var fee = 7000;
+            var myGPA = 0.00;
+            var ch = 'n';
+            var name = "";
+            bool decision = false;
+            decimal myDec = 0.0m;
+            var GPA = "";
+            var sem="";
+            var dis = 0;
+        }
+    }
+}
